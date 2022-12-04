021076ed43f821d2a69c42eb7c03b3e8402a5dd3
Prex-san <raghuraman417@gmail.com>
2022-12-05 03:19:15 +0530

    Update images and add animated images
    The hover effect in the services page will now bring up gifs.
    Broken image links fixed with relative links.
    JavaScript updated with hover() and unhover functions.

--- homepage/services-page/resources/index.js
+++ homepage/services-page/resources/index.js
@@ -9,3 +9,43 @@ document.getElementById("update-2").innerHTML = "Appointments remaining:" + " " 
 document.getElementById("update-3").innerHTML = "Appointments remaining:" + " " + superDeluxeExorcism;
 document.getElementById("update-4").innerHTML = "Appointments remaining:" + " " + experienceParadise;
 document.getElementById("update-5").innerHTML = "Appointments remaining:" + " " + fightEspers;
+
+function hover(element,num) {
+    switch(num){
+        case 1:
+            element.setAttribute('src', '../../resources/salt.gif');
+            break;
+        case 2:
+            element.setAttribute('src', '../../resources/reigen-massage.gif');
+            break;
+        case 3:
+            element.setAttribute('src', '../../resources/Reigenknee.webp');
+            break;
+        case 4:
+            element.setAttribute('src', '../../resources/special-massage.gif');
+            break;
+        case 5:
+            element.setAttribute('src', '../../resources/dropkick.gif');
+            break;
+    }
+  }
+  
+  function unhover(element,num) {
+    switch(num){
+        case 1:
+            element.setAttribute('src', '../../resources/salt-punch.webp');
+            break;
+        case 2:
+            element.setAttribute('src', '../../resources/massage.jpg');
+            break;
+        case 3:
+            element.setAttribute('src', '../../resources/knee-kick.jpg');
+            break;
+        case 4:
+            element.setAttribute('src', '../../resources/special-massage.jpg');
+            break;
+        case 5:
+            element.setAttribute('src', '../../resources/esper-kick.webp');
+            break;
+    }
+  }
