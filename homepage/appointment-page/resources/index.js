16dbc3daf31f102221a3487597f4f181c07afe6f
Raghu Raman <raghuraman417@gmail.com>
2022-12-05 09:45:17 +0530

    Fix negative count in the services page

--- homepage/appointment-page/resources/index.js
+++ homepage/appointment-page/resources/index.js
@@ -18,46 +18,51 @@ function onSubmit(){
     console.log(courseName);
     if(courseName == "ordinary-exorcism")
     {
-        ordinaryExorcism--;
-        localStorage.setItem('ordinary-exorcism', ordinaryExorcism);
-        if(ordinaryExorcism > 0)
+        if(ordinaryExorcism > 0){
             document.getElementById("message").innerHTML = "The course you've selected is " + courseName+".";
+            ordinaryExorcism--;
+            localStorage.setItem('ordinary-exorcism', ordinaryExorcism);
+        }
         else
             document.getElementById("message").innerHTML = "Unfortunately, "+ courseName +" is not available.";
     }
     else if(courseName == "special-exorcism")
    { 
-        specialExorcism--;
-        localStorage.setItem('special-exorcism',specialExorcism);
-        if(specialExorcism > 0)
+        if(specialExorcism > 0){
             document.getElementById("message").innerHTML = "The course you've selected is " + courseName+".";
+            specialExorcism--;
+            localStorage.setItem('special-exorcism',specialExorcism);
+        }
         else
             document.getElementById("message").innerHTML = "Unfortunately, "+ courseName +" is not available.";
     }
     else if(courseName == "super-deluxe-exorcism")
     { 
-        superDeluxeExorcism--;
-        localStorage.setItem('super-deluxe-exorcism', superDeluxeExorcism);
-        if(superDeluxeExorcism > 0)
+        if(superDeluxeExorcism > 0){
             document.getElementById("message").innerHTML = "The course you've selected is " + courseName+".";
+            superDeluxeExorcism--;
+            localStorage.setItem('super-deluxe-exorcism', superDeluxeExorcism);
+        }
         else
             document.getElementById("message").innerHTML = "Unfortunately, "+ courseName +" is not available.";
     }
     else if(courseName == "experience-paradise") 
     {
-        experienceParadise--;
-        localStorage.setItem('experience-paradise', experienceParadise);
-        if(experienceParadise > 0)
+        if(experienceParadise > 0){
             document.getElementById("message").innerHTML = "The course you've selected is " + courseName+".";
+            experienceParadise--;
+            localStorage.setItem('experience-paradise', experienceParadise);
+        }
         else
             document.getElementById("message").innerHTML = "Unfortunately, "+ courseName +" is not available.";
     }
     else if(courseName == "fight-other-espers")
     {
-        fightEspers--;
-        localStorage.setItem('fight-other-espers', fightEspers);
-        if(fightEspers > 0)
+        if(fightEspers > 0){
             document.getElementById("message").innerHTML = "The course you've selected is " + courseName+".";
+            fightEspers--;
+            localStorage.setItem('fight-other-espers', fightEspers);
+        }
         else
             document.getElementById("message").innerHTML = "Unfortunately, "+ courseName +" is not available.";
     }
